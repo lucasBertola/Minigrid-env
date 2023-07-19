4124d76d7dbe407ff473116524452535c99e6bdf
Lucas Bertola <lucasbertola@gmail.com>
2023-07-19 23:30:24 +0200

    add package pip

---
+++ gymnasium_minigrid/__init__.py
@@ -0,0 +1,1 @@
+from .MiniGridEnv import MiniGridEnv
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='gymnasium_minigrid',
+    version='0.0.4',
+    description='A mini grid environment for OpenAI Gym and Gymnasium',
+    author='Lucas Bertola',
+    url='https://github.com/lucasBertola/Minigrid',  
+    # author_email='your.email@example.com',
+    packages=find_packages(),
+    install_requires=[
+        'pygame==2.1.3',
+        'gymnasium==0.28.1',
+    ]
+)
